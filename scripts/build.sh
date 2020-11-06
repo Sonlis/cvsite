74e95dc064a051366d0e0ac8b746e0b5039d854e
= <=>
2020-11-06 13:13:40 +0100

    Added hook to auto-build dockerfile

---
+++ scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+set -e 
+
+if [[ ${BRANCH} == master ]]; then
+    IMAGE = bastibast/${APP_NAME}:${APP_NAME}
+    docker build ../ -t ${IMAGE}
+
+elif [[ ${BRANCH} == develop* ]]; then
+    IMAGE = bastibast/${APP_NAME}:${REVISION}
+    docker build ../ -t ${IMAGE}
+else
+    echo "App not found"
+    exit 1
+fi
+
+docker push ${IMAGE}
