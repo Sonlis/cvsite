a0e5e80397ebb7e750d1bef9c6f6fda1207c4d13
= <=>
2020-11-09 10:50:59 +0100

    Try fixing variables name

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,16 @@
 
 set -e 
 
-if [[ ${BRANCH} == master ]]; then
-    IMAGE = bastibast/${APP_NAME}:${APP_NAME}
-    docker build ../ -t ${IMAGE}
+if [[ $(BRANCH) == master ]]; then
+    IMAGE = bastibast/$(APP_NAME):$(APP_NAME)
+    docker build ../ -t $(IMAGE)
 
-elif [[ ${BRANCH} == develop* ]]; then
-    IMAGE = bastibast/${APP_NAME}:${REVISION}
-    docker build ../ -t ${IMAGE}
+elif [[ $(BRANCH) == develop* ]]; then
+    IMAGE = bastibast/$(APP_NAME):$(REVISION)
+    docker build ../ -t $(IMAGE)
 else
     echo "App not found"
     exit 1
 fi
 
-docker push ${IMAGE}
+docker push $(IMAGE)
