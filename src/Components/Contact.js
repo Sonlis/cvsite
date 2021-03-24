7c9fc0142bb00bb4873aa495df1a674ea319715a
Sonlis <bastien.jeannelle@gmail.com>
2021-03-24 15:39:36 +0100

    Hotfixes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -78,7 +78,7 @@ class Contact extends Component {
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
+					   <h4>Contact details</h4>
 					   <p className="address">
 						   {name}<br />
 						   {city}, {state} {zip}<br />
